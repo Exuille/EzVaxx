c1f89029c0f8f10763dd247b62abb164db625ded
Exuille <apenuliar75@gmail.com>
2025-04-09 19:24:53 +0800

    Add auth controller,model, and route

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import {register, login, protect, logout, fetchUser, updatePass} from '../controllers/authController.js';
+
+const router = express.Router()
+
+router.route("/register").post(signup);
+router.route("/login").post(login);
+router.route("/logout").post(protect, logout);
+router.route("/fetch").get(protect, fetchUser);
+router.route("/update/password").post(protect, updatePass)
+
+export default router;
